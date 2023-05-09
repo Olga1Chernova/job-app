dfcd8b9097bca28a030e835f76537915825b2601
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-05-10 00:06:46 +0300

    Update useFetch.js

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,45 +4,51 @@ import { RAPID_API_KEY } from '@env';
 
 const rapidApiKey = RAPID_API_KEY;
 
-const useFetch = (endpoint, query) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    
-    const options = {
-        method: 'GET',
-        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-        headers: {
-            'X-RapidAPI-Key': rapidApiKey,
-            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-        },
-        params: {...query}
-    }
-    
-    const fetchData = async () => {
-        setIsLoading(true);
-        
-        try {
-            const response = await axios.request(options);
-            setData(response.data.data);
-            setIsLoading(false);
-        } catch (error) {
-            setError(error.message);
-            alert(error.message)
-        } finally {
-            setIsLoading(false);
-        }
-    }
+import throttle from "lodash.throttle";
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+const useFetch = (endpoint, query, delay) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const refetch = () => {
-        setIsLoading(true);
-        fetchData()
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'X-RapidAPI-Key': rapidApiKey,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query }
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+      alert('Error happened');
+    } finally {
+      setIsLoading(false);
     }
+    };
 
-    return { data, isLoading, error, refetch };
-}
+  // Throttle the fetchData function to limit the request rate
+  const throttledFetchData = throttle(fetchData, delay);
+
+  useEffect(() => {
+    throttledFetchData();
+    // Cancel the throttled function on unmount
+    return () => {
+      throttledFetchData.cancel();
+    };
+  }, []);
+
+  return { data, isLoading, error };
+};
+
 export default useFetch;

2400b670892798285798d98a1ab6a5a328bab60a
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-05-09 18:01:09 +0300

    created a custom useFetch hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -8,7 +8,7 @@ const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-
+    
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
@@ -23,11 +23,26 @@ const useFetch = (endpoint, query) => {
         setIsLoading(true);
         
         try {
-            
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
         } catch (error) {
-            
+            setError(error.message);
+            alert(error.message)
         } finally {
-            
+            setIsLoading(false);
         }
     }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData()
+    }
+
+    return { data, isLoading, error, refetch };
 }
+export default useFetch;

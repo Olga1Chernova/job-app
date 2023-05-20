5bf3b1e774e9ad257aeb5940b9cae55f3083cbf7
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-05-20 22:17:55 +0300

    updated useFetch hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -2,21 +2,19 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { RAPID_API_KEY } from '@env';
 
-const rapidApiKey = RAPID_API_KEY;
-
 import throttle from "lodash.throttle";
 
 const useFetch = (endpoint, query, delay) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-
+  
   const options = {
     method: 'GET',
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      'X-RapidAPI-Key': rapidApiKey,
-      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      'X-RapidAPI-Key': RAPID_API_KEY,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
     },
     params: { ...query }
   };
